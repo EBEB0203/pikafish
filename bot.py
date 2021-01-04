37b7729632938c4cf09a49eb6a90769f9078cbd3
EB EB <74333830+EBEB0203@users.noreply.github.com>
2021-01-04 20:36:23 +0800

    bot
    json

--- bot.py
+++ bot.py
@@ -3,6 +3,10 @@ from discord.ext import commands
 import random
 import json
 
+#呼叫Json
+with open('setting.json', 'r', encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
 bot = commands.Bot(command_prefix = ">")
 
 @bot.event
@@ -18,18 +22,17 @@ async def 幸運指數(ctx):
     await ctx.send(F'{random.random(1,100)} ('%')')
 
 
-'''
 @bot.event
 async def on_member_join(member):
-    channel = bot.git_channel(795616804578787338)
+    channel = bot.git_channel(int(jdata['Welcome_channel']))
     await channel.send(F'{member} join!')
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.git_channel(795616890630176788)
+    channel = bot.git_channel(int(jdata['Leave_channel']))
     await channel.send(F'{member} leave!')
-'''
 
-bot.run("Nzk1NTk4NTY5MDE2MDAwNTUy.X_Ls6A.3bz7KI80ae6jSHOCz2mYJnRHMHs")
+
+bot.run(jdata['TOKEN'])
 
 
